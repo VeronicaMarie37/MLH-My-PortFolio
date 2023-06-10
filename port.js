be28c04ffdc662ba11668fd48cb452bbb34ebbb2
MelvinLewis <veronica.m.hannah@gmail.com>
2023-06-10 15:00:42 -0400

    //added images finished home page

--- port.js
+++ port.js
@@ -1,1 +1,60 @@
+$( () => {
+  
+  
 
+
+    
+    //hover is attached to my li with the id of content
+    // then there are two functions use when the mouse is over the list
+    //when the mouse leaves the list
+    
+    
+    
+    
+    // $this represent that current dom element that is being selected or worked on
+    // in the function in this case
+    //content li is the element
+    //hover is the event handler
+    
+    
+     
+ $('#sidebar li').hover(
+    function() {
+     $(this).css('background-color', 'black');
+    },
+
+    function() {
+    $(this).css('background-color', 'none');
+     }
+); 
+    
+    
+    
+
+
+
+
+
+
+
+$(document).ready(function() {
+    var $sidebar = $("#sidebar");
+    var $hamburgerMenu = $("#hamburgerMenu");
+    var $closeIcon = $("#closeIcon");
+  
+    $hamburgerMenu.click(function() {
+      $sidebar.css("left", "0");
+      $hamburgerMenu.toggleClass("open");
+      $closeIcon.toggleClass('open')
+    });
+  
+    $closeIcon.click(function() {
+      $sidebar.css("left", "-300px");
+      $hamburgerMenu.toggleClass("open");
+      $closeIcon.toggleClass('open')
+    });
+  });
+
+ 
+  
+})
